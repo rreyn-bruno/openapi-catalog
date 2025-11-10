597f5ecd507b41037945a8c3bd597620b7237be2
rreyn-bruno <ryan@usebruno.com>
2025-11-10 17:03:52 -0600

    Remove bruno-doc-gen directory to prepare for submodule

--- bruno-doc-gen/src/vendored-bruno-lang/v2/src/dotenvToJson.js
+++
@@ -1,9 +0,0 @@
-const dotenv = require('dotenv');
-
-const parser = (input) => {
-  const buf = Buffer.from(input);
-  const parsed = dotenv.parse(buf);
-  return parsed;
-};
-
-module.exports = parser;
